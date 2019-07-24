47d7e6a4e86b3406057b2efc759c97eef3a3455d
Dina Leah <dinousar123@gmail.com>
2019-07-24 17:15:51 -0400

    Newest Changes!!!

--- app/src/main/java/com/example/salecalculator/MainActivity.java
+++ app/src/main/java/com/example/salecalculator/MainActivity.java
@@ -2,11 +2,14 @@ package com.example.salecalculator;
 
 import android.content.Intent;
 import android.os.Bundle;
+
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.BaseTransientBottomBar;
 import com.google.android.material.snackbar.Snackbar;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -19,108 +22,103 @@ import static com.example.salecalculator.R.id.et_original;
 
 public class MainActivity extends AppCompatActivity {
 
-        private View mSBcontainer;
-        private Snackbar mSnackBar;
+    private View mSBcontainer;
+    private Snackbar mSnackBar;
 
-        private EditText original;
-        private EditText disc;
-        private double mOriginalPrice, mPercentDiscount, mSaved, mTotal;
+    private EditText original;
+    private EditText disc;
+    private double mOriginalPrice, mPercentDiscount, mSaved, mTotal;
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-                super.onCreate(savedInstanceState);
-                setContentView(layout.activity_main);
-                Toolbar toolbar = findViewById(id.toolbar);
-                setSupportActionBar(toolbar);
-               original = findViewById(id.et_original);
-              disc = findViewById(id.et_discount);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(layout.activity_main);
+        Toolbar toolbar = findViewById(id.toolbar);
+        setSupportActionBar(toolbar);
+        original = findViewById(id.et_original);
+        disc = findViewById(id.et_discount);
 
-                FloatingActionButton fab = findViewById(id.fab);
+        FloatingActionButton fab = findViewById(id.fab);
 
 
-                fab.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View view) {
-                                mOriginalPrice=getOriginalPrice();
-                                mPercentDiscount=getPercentDiscount();
-                                mSaved=calculateSaved(mOriginalPrice,mPercentDiscount);
-                                mTotal=calculateTotal(mOriginalPrice, mSaved);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mOriginalPrice = getOriginalPrice();
+                mPercentDiscount = getPercentDiscount();
+                mSaved = calculateSaved(mOriginalPrice, mPercentDiscount);
+                mTotal = calculateTotal(mOriginalPrice, mSaved);
 
-                                Intent intent = new Intent(getApplicationContext(),ResultsActivity.class);
-                                intent.putExtra("ORIGINAL_PRICE",mOriginalPrice);
-                                intent.putExtra("PERCENT_DISCOUNT",mPercentDiscount);
-                                intent.putExtra("SAVED",mSaved);
-                                intent.putExtra("TOTAL",mTotal);
-                                startActivity(intent);
-                        }
-                });
+                Intent intent = new Intent(getApplicationContext(), ResultsActivity.class);
+                intent.putExtra("ORIGINAL_PRICE", mOriginalPrice);
+                intent.putExtra("PERCENT_DISCOUNT", mPercentDiscount);
+                intent.putExtra("SAVED", mSaved);
+                intent.putExtra("TOTAL", mTotal);
+                startActivity(intent);
+            }
+        });
 
+    }
+
+
+    private double getOriginalPrice() {
+
+        double mOriginalPrice = Double.parseDouble(original.getText().toString());
+        if (mOriginalPrice < 0) {
+            Snackbar.make(mSBcontainer, "The original price must be greater than zero", Snackbar.LENGTH_LONG).show();
         }
 
+        return mOriginalPrice;
+    }
 
-        private double getOriginalPrice() {
-
-                double mOriginalPrice=Double.parseDouble(original.getText().toString());
-                if (mOriginalPrice<0)
-                {
-                        Snackbar.make(mSBcontainer,"The original price must be greater than zero",Snackbar.LENGTH_LONG).show();
-                }
-
-                return mOriginalPrice;
+    private double getPercentDiscount() {
+        double mPercentDiscount = Double.parseDouble(disc.getText().toString());
+        if (mPercentDiscount < 0) {
+            Snackbar.make(mSBcontainer, "The percent discount must be greater than 0%", Snackbar.LENGTH_LONG).show();
+        } else if (mPercentDiscount > 100) {
+            Snackbar.make(mSBcontainer, "The percent discount must be less than 100%", Snackbar.LENGTH_LONG).show();
         }
-
-        private double getPercentDiscount()
-        {
-                double mPercentDiscount=Double.parseDouble(disc.getText().toString());
-                if (mPercentDiscount<0)
-                {
-                        Snackbar.make(mSBcontainer,"The percent discount must be greater than 0%",Snackbar.LENGTH_LONG).show();
-                }
-                else if (mPercentDiscount>100)
-                {
-                        Snackbar.make(mSBcontainer,"The percent discount must be less than 100%",Snackbar.LENGTH_LONG).show();
-                }
-                return mPercentDiscount;
-        }
+        return mPercentDiscount;
+    }
 
 
+    private double calculateTotal(double mOriginalPrice, double mSaved) {
+        double mTotal = mOriginalPrice - mSaved;
+        return mTotal;
+    }
 
-        private double calculateTotal(double mOriginalPrice, double mSaved) {
-                double mTotal=mOriginalPrice-mSaved;
-                return mTotal;
-        }
+    private double calculateSaved(double mOriginalPrice, double mPercentDiscount) {
+        double mSaved = (mPercentDiscount / 100) * mOriginalPrice;
 
-        private double calculateSaved(double mOriginalPrice, double mPercentDiscount) {
-                double mSaved=(mPercentDiscount/100)*mOriginalPrice;
-
-                return mSaved;
-        }
-
-
-        @Override
-        public boolean onCreateOptionsMenu(Menu menu) {
-                // Inflate the menu; this adds items to the action bar if it is present.
-                getMenuInflater().inflate(R.menu.menu_main, menu);
-                return true;
-        }
-
-        @Override
-        public boolean onOptionsItemSelected(MenuItem item) {
-                // Handle action bar item clicks here. The action bar will
-                // automatically handle clicks on the Home/Up button, so long
-                // as you specify a parent activity in AndroidManifest.xml.
-                int id = item.getItemId();
-
-                //noinspection SimplifiableIfStatement
-                if (id == R.id.action_settings) {
-                        return true;
-                }
-
-                return super.onOptionsItemSelected(item);
-        }
+        return mSaved;
+    }
 
 
-        public void showAbout(MenuItem item) {
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
 
-        }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+
+
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    public void showAbout(MenuItem item) {
+
+        Utils.showInfoDialog(this, "About Menu", "Created by Dina Leah Garber, Chana Lavian," +
+                "Laya KleinKaufman C 2019-Proffessor Abromson's class Summer 2019!");
+
+    }
 }
--- app/src/main/java/com/example/salecalculator/ResultsActivity.java
+++ app/src/main/java/com/example/salecalculator/ResultsActivity.java
@@ -1,10 +1,13 @@
 package com.example.salecalculator;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
@@ -16,6 +19,7 @@ public class ResultsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_results);
         setUpFab();
+        setUpToolbar();
         mOriginalPrice = findViewById(R.id.tv_orig);
         mPercentDiscount=findViewById(R.id.tv_discount);
         mSaved=findViewById(R.id.tv_saved);
@@ -25,6 +29,29 @@ public class ResultsActivity extends AppCompatActivity {
 
     }
 
+    private void setUpToolbar() {
+        Toolbar toolbar = findViewById (R.id.toolbar);
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_results, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if(id==android.R.id.home){
+            finish();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
     private void setUpFab() {
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener(){
@@ -52,4 +79,8 @@ public class ResultsActivity extends AppCompatActivity {
     }
 
 
+    public void showAbout(MenuItem item) {
+        Utils.showInfoDialog(this,"About Menu","Created by Dina Leah Garber, Chana Lavian,+" +
+                "Laya KleinKaufman C 2019-Proffessor Abromson's class Summer 2019!");
+    }
 }
